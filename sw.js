af08148381c306efbfcb83adecaf84c129e8fcbf
drillerguy <mikew@s-ug.net>
2026-07-23 14:43:03 -0500

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE='foundation-inspector-pro-v1';
-const CORE=['./','./index.html','./manifest.webmanifest','../-ORD-Caisson-Inspector/caisson-plan.png','../-ORD-Caisson-Inspector/index.html'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{const copy=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return resp}).catch(()=>caches.match('./index.html')))));
